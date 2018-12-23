7e2dbcacb4b2a55820788e41996003dfba3d45b4
PedroPini <augustopini@gmail.com>
2018-12-23 21:26:49 -0200

    Adding Design pattern project

---
+++ DesingPattern/Strategy/CursoDesignPatterns/Orcamento.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoDesignPatterns
+{
+    public class Orcamento
+    {
+        public Orcamento(double valor)
+        {
+            this.Valor = valor;
+        }
+        public double Valor { get; private set; }
+    }
+}
