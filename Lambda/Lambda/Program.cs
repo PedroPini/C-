f9c2761c604fd3f922a377a2ae7a896ade756bab
PedroPini <augustopini@gmail.com>
2019-01-06 10:03:20 -0200

    Adding projects Lambda and SockMerchant

---
+++ Lambda/Lambda/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lambda
+{
+    class Program
+    {
+       delegate void operacao(int x, int y);
+        static void Main(string[] args)
+        {
+            operacao divi = (x, y) =>
+            {
+                Console.WriteLine("DIVISAO");
+                Console.WriteLine(x / y);
+            };
+            divi(6, 3);
+            divi(8, 2);
+
+            divi = (x, y) =>
+            {
+                Console.WriteLine("Multiplicacao");
+                Console.WriteLine(x * y);
+            };
+            divi(6, 3);
+
+            //acha o indice do elemento impar na lista
+            List<int> elements = new List<int>() { 10, 20, 40, 31 };
+            int oddIndex = elements.FindIndex(elementos => elementos % 2 != 0);
+            Console.WriteLine("\nLISTA");
+            Console.WriteLine(oddIndex);
+            //Console.ReadLine();
+        }
+    }
+}
---
+++ SockMerchant/SockMerchant/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the sockMerchant function below.
+    static int sockMerchant(int n, int[] ar)
+    {
+
+        int pairsFound = 0;
+        var sockColorHash = new Dictionary<int, int>();
+
+        foreach (var sock in ar)
+        {
+            if (sockColorHash.ContainsKey(sock))
+            {
+                pairsFound++;
+                sockColorHash.Remove(sock);
+            }
+            else
+                sockColorHash.Add(sock, 1);
+        }
+        return pairsFound;
+
+
+
+
+    }
+
+    static void Main(string[] args)
+    {
+        
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] ar = Console.ReadLine().Trim().Split(' ').Select(x => Int32.Parse(x)).ToArray();
+        int result = sockMerchant(n, ar);
+
+       
+    }
+}
